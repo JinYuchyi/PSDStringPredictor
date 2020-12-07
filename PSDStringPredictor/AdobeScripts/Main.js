408aed3b4e0be5436de6513c6f1b307444416696
Yuqi Jin <yuqi_jin@icloud.com>
2020-12-07 10:47:11 +0800

    Adobe script.

---
+++ PSDStringPredictor/AdobeScripts/Main.js
@@ -0,0 +1,69 @@
+
+// ========================= Load from Main.js ==============================
+
+//PS Preferance Setting
+
+var originalUnit = preferences.rulerUnits
+preferences.rulerUnits = Units.PIXELS
+
+var fileRef = File(psdPath)
+var docRef = app.open(fileRef)
+
+//Create Folder
+var len = docRef.layerSets.length
+
+//Remove the previous folder
+var i
+if(len >= 1){
+    for (i = 0; i < len; i++){
+        if (docRef.layerSets[i].name == "StringLayerGroup"){
+            docRef.layerSets[i].remove()
+        }
+    }
+}
+
+//Add new folder
+var layerSetRef = app.activeDocument.layerSets.add()
+layerSetRef.name = "StringLayersGroup"
+var layerSetRef1 = app.activeDocument.layerSets.add()
+layerSetRef1.name = "MaskLayersGroup"
+
+
+//Add Text layer
+const num = contentList.length
+for (var i = 0; i < num; i++){
+    var artLayerRef = layerSetRef.artLayers.add()
+    artLayerRef.kind = LayerKind.TEXT
+    var textItemRef = artLayerRef.textItem
+    textItemRef.contents = contentList[i]
+    textColor = new SolidColor
+    textColor.rgb.red = colorList[i][0]
+    textColor.rgb.green = colorList[i][1]
+    textColor.rgb.blue = colorList[i][2]
+    textItemRef.color = textColor
+    textItemRef.font = fontNameList[i]
+    textItemRef.size = new UnitValue(fontSizeList[i], "pt")
+    var alignmentOffset = 0
+    var alignName = "left"
+    if (alignmentList[i] == 1) {
+        alignmentOffset = rectList[i][2] / 2
+        alignName = "center"
+    }
+    if (alignmentList[i] == 2) {
+        alignmentOffset = rectList[i][2]
+        alignName = "right"
+    }
+
+    textItemRef.position = Array(positionList[i][0] - offsetList[i][0] + alignmentOffset, positionList[i][1]  - offsetList[i][1] / 4)
+    textItemRef.tracking = trackingList[i]
+    artLayerRef.name = names[i]
+    selectLayer(artLayerRef.name)
+    setTextAlignment(alignName)
+}
+
+//Add BG Layer
+for (var i = 0; i < num; i++){
+    offset = 5
+    textColor = bgColorList[i]
+    createRectangle(layerSetRef1, names[i], positionList[i][0] - offset,positionList[i][1] - rectList[i][3] - offset, rectList[i][2] + offset, rectList[i][3] + offset, textColor)
+}
